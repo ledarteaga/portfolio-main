8c164c8d50be256d09ed37f8780d857c6ad42826
ledarteaga <larteaga@boxmis.com>
2023-12-25 22:37:15 -0500

    se implementa formulario de contacto

---
+++ components/ContactContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="h-[50vh] w-1/3 m-auto my-20">
+    <h3 class="font-bold text-3xl  leading-loose">
+      Contact me
+    </h3>
+
+    <UForm :state="{}" class="space-y-4">
+      <UFormGroup label="Name" name="name">
+        <UInput />
+      </UFormGroup>
+      <UFormGroup label="Email" name="email">
+        <UInput />
+      </UFormGroup>
+
+      <UFormGroup label="Message" name="message">
+        <UTextarea />
+      </UFormGroup>
+
+      <UButton type="submit" class="w-full text-center">
+        Submit
+      </UButton>
+    </UForm>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class=" bg-gradient-to-b from-black to-[#44a8b3]/30 from-60%">
     <HeroContent />
     <RepositoriesWrapper />
+    <ContactContent />
     <FooterContent />
   </div>
 </template>
